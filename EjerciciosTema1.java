3e0f870da7f0ce8742b85ad86711c6dd8f906a5d
igonzalonicolas <igonzalonicolas@gmail.com>
2022-10-18 15:00:11 -0300

    Java tema 1

---
+++ EjerciciosTema1.java
@@ -0,0 +1,19 @@
+package EjerciciosTema1;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+
+        int variable1 = 5;
+        long variable2 = 2651;
+        double variable3 = 4.5;
+        boolean variable4 = false;
+        String variable5 = "Open Bootcamp";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+    }
+}
